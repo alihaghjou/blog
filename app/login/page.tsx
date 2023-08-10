725021c8824792a437cdd036a8ae5d6f1a65d121
alihaghjou <alihaghjou30@gmail.com>
2023-08-10 14:26:23 +0330

    created alert for login and added logout status

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,51 +1,72 @@
-"use client"
+"use client";
 
-import { useState, useEffect } from 'react'
-import { useRouter } from 'next/navigation'
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import Alert from "./Alert";
 
 export default function Login() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [view, setView] = useState('sign-in')
-  const router = useRouter()
-  const supabase = createClientComponentClient()
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [view, setView] = useState("sign-in");
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const router = useRouter();
+  const supabase = createClientComponentClient();
 
   useEffect(() => {
-    checkLogged()
-  }, [])
+    checkLogged();
+  }, []);
+
+  const openAlert = (status: string) => {
+    if (status === "success") {
+      setIsSuccess(true);
+      setTimeout(() => setIsSuccess(false), 3000);
+    } else if (status === "error") {
+      setIsError(true);
+      setTimeout(() => setIsError(false), 3000);
+    }
+  };
 
   const checkLogged = async () => {
-    const {data: {user}} = await supabase.auth.getUser()
-    console.log(user)
-    if (user) router.push("/account")
-  }
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+    console.log(user);
+    if (user) router.push("/account");
+  };
 
   const handleSignUp = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
+    e.preventDefault();
     await supabase.auth.signUp({
       email,
       password,
       options: {
         emailRedirectTo: `${location.origin}/auth/callback`,
       },
-    })
-    setView('check-email')
-  }
+    });
+    setView("check-email");
+  };
 
   const handleSignIn = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    await supabase.auth.signInWithPassword({
+    e.preventDefault();
+    const { data, error } = await supabase.auth.signInWithPassword({
       email,
       password,
-    })
-    router.push('/')
-    router.refresh()
-  }
+    });
+    if (error) {
+      openAlert("error");
+    }
+    if (data.user !== null) {
+      openAlert("success");
+      router.push("/account");
+      router.refresh();
+    }
+  };
 
   return (
     <main className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
-      {view === 'check-email' ? (
+      {view === "check-email" ? (
         <p className="text-center text-foreground">
           Check <span className="font-bold">{email}</span> to continue signing
           up
@@ -53,7 +74,7 @@ export default function Login() {
       ) : (
         <form
           className="flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
-          onSubmit={view === 'sign-in' ? handleSignIn : handleSignUp}
+          onSubmit={view === "sign-in" ? handleSignIn : handleSignUp}
         >
           <label className="text-md" htmlFor="email">
             Email
@@ -76,7 +97,7 @@ export default function Login() {
             value={password}
             placeholder="••••••••"
           />
-          {view === 'sign-in' && (
+          {view === "sign-in" && (
             <>
               <button className="bg-green-700 rounded px-4 py-2 text-white mb-6">
                 Sign In
@@ -85,14 +106,14 @@ export default function Login() {
                 Don't have an account?
                 <button
                   className="ml-1 underline"
-                  onClick={() => setView('sign-up')}
+                  onClick={() => setView("sign-up")}
                 >
                   Sign Up Now
                 </button>
               </p>
             </>
           )}
-          {view === 'sign-up' && (
+          {view === "sign-up" && (
             <>
               <button className="bg-green-700 rounded px-4 py-2 text-white mb-6">
                 Sign Up
@@ -101,7 +122,7 @@ export default function Login() {
                 Already have an account?
                 <button
                   className="ml-1 underline"
-                  onClick={() => setView('sign-in')}
+                  onClick={() => setView("sign-in")}
                 >
                   Sign In Now
                 </button>
@@ -110,6 +131,7 @@ export default function Login() {
           )}
         </form>
       )}
+      <Alert isError={isError} isSuccess={isSuccess} />
     </main>
-  )
+  );
 }
--- components/LogoutButton.tsx
+++ components/LogoutButton.tsx
@@ -1,25 +1,31 @@
-'use client'
+"use client";
 
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
-import { useRouter } from 'next/navigation'
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function LogoutButton() {
-  const router = useRouter()
+  const router = useRouter();
+  const [isLoggingOut, setIsloggingOut] = useState(false)
 
   // Create a Supabase client configured to use cookies
-  const supabase = createClientComponentClient()
+  const supabase = createClientComponentClient();
 
   const signOut = async () => {
-    await supabase.auth.signOut()
-    router.refresh()
-  }
+    setIsloggingOut(true)
+    const log = await supabase.auth.signOut();
+    setIsloggingOut(false)
+    router.push("/");
+    router.refresh();
+  };
 
   return (
     <button
-    className="ring-1 ring-green-500 rounded px-4 py-2 hover:bg-green-500 hover:text-green-100"
+      className="ring-1 ring-green-500 rounded px-4 py-2 hover:bg-green-500 hover:text-green-100"
       onClick={signOut}
+      disabled={isLoggingOut}
     >
-      Logout
+      {isLoggingOut ? <span>Loading...</span> : <span>Logout</span>}
     </button>
-  )
+  );
 }
