2d5e79a3d65b83345e1ac62c93506cc2c2b0374a
alihaghjou <alihaghjou30@gmail.com>
2023-10-12 22:02:30 +0330

    welcome component with all css libraries

---
+++ app/Welcome.tsx
@@ -0,0 +1,40 @@
+"use client";
+import { Disclosure, Transition } from "@headlessui/react";
+import { ChevronRightIcon } from "@heroicons/react/20/solid";
+import { useState } from "react";
+
+export default function Welcome() {
+  const [open, setOpen] = useState(false);
+  return (
+    <Disclosure>
+      {({ open }) => (
+        <>
+          <Disclosure.Button
+            className="text-center text-sky-800 font-semibold text-lg my-4 flex justify-center items-center w-full"
+            onClick={() => setOpen((perv) => !perv)}
+          >
+            Know About Our Goal{" "}
+            <ChevronRightIcon
+              height={50}
+              className={open ? "rotate-90 transform" : ""}
+            />
+          </Disclosure.Button>
+
+          <Transition
+            enter="transition duration-100 ease-out"
+            enterFrom="transform scale-95 opacity-0"
+            enterTo="transform scale-100 opacity-100"
+            leave="transition duration-75 ease-out"
+            leaveFrom="transform scale-100 opacity-100"
+            leaveTo="transform scale-95 opacity-0"
+          >
+            <Disclosure.Panel>
+              Yes! You can purchase a license that you can share with your
+              entire team.
+            </Disclosure.Panel>
+          </Transition>
+        </>
+      )}
+    </Disclosure>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { Database } from "@/supabase";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import Link from "next/link";
+import Welcome from "./Welcome";
 
 export type postType = {
   id: number;
@@ -29,22 +30,25 @@ export default async function Index() {
   const sortedPosts = sortPostsByDate(posts);
 
   return (
-    <main className="m-auto md:w-4/5 lg:w-3/4 w-full min-h-screen animate-in">
-      {sortedPosts.map((post) => (
-        <article key={post.id} className="border-b p-6 flex flex-col gap-3">
-          <h1 className="text-2xl font-bold py-2 capitalize">{post.name}</h1>
-          <h2 className="text-sm text-gray-600 py-2">
-            <span className="font-semibold">Updated At:</span>{" "}
-            {new Date(post.updated_at).toDateString()}
-          </h2>
-          <p className="line-clamp-2 indent-2 leading-6">{post.content}</p>
-          <Link className="self-end" href={`/${post.id}`}>
-            <button className="text-cyan-800 ring-1 ring-cyan-300 rounded py-2 px-4  hover:bg-cyan-400 hover:text-white">
-              Continue Reading
-            </button>
-          </Link>
-        </article>
-      ))}
-    </main>
+    <>
+      <main className="m-auto md:w-4/5 lg:w-3/4 w-full min-h-screen animate-in relative">
+        <Welcome />
+        {sortedPosts.map((post) => (
+          <article key={post.id} className="border-b p-6 flex flex-col gap-3">
+            <h1 className="text-2xl font-bold py-2 capitalize">{post.name}</h1>
+            <h2 className="text-sm text-gray-600 py-2">
+              <span className="font-semibold">Updated At:</span>{" "}
+              {new Date(post.updated_at).toDateString()}
+            </h2>
+            <p className="line-clamp-2 indent-2 leading-6">{post.content}</p>
+            <Link className="self-end" href={`/${post.id}`}>
+              <button className="text-cyan-800 ring-1 ring-cyan-300 rounded py-2 px-4  hover:bg-cyan-400 hover:text-white">
+                Continue Reading
+              </button>
+            </Link>
+          </article>
+        ))}
+      </main>
+    </>
   );
 }
